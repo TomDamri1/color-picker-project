07d7e734f6a196402dde1f316d5f3f85e9e1aa54
TomDamri1 <tomdamri@gmail.com>
2019-09-21 00:39:14 +0300

    Added - Palette , ColorBox

---
+++ color-picker-project/src/ColorBox/ColorBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './ColorBox.css'
+export class ColorBox extends Component {
+    render() {
+        return (
+            <div className="ColorBox" style={{backgroundColor : this.props.background}}>
+                <span>{this.props.name}</span>
+
+            </div>
+        )
+    }
+}
+
+export default ColorBox
