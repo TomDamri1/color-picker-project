cfda5917b2f7b2c1dc159770d93742c3ce1d37b0
TomDamri1 <tomdamri@gmail.com>
2019-09-21 13:01:52 +0300

    added - copy to clipboard by clicking on a ColorBox

--- color-picker-project/src/ColorBox/ColorBox.js
+++ color-picker-project/src/ColorBox/ColorBox.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import './ColorBox.css'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
 export class ColorBox extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: '',
+            copied: false,
+        }
+    }
     render() {
         return (
+            <CopyToClipboard 
+            text={this.props.background}>
             <div 
             className="ColorBox" 
             style={{backgroundColor : this.props.background}}>
@@ -10,10 +21,11 @@ export class ColorBox extends Component {
                     <div className="box-content">
                         <span>{this.props.name}</span>
                     </div>
-                    <button className="copy-button">Copy!</button>
+                   <button className="copy-button">Copy!</button>
                 </div>
                 <span className="see-more">More</span>
             </div>
+            </CopyToClipboard>
         )
     }
 }
