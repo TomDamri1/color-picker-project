73deca713a32b9397d9a6eea7fa74c07e1a8a151
TomDamri1 <tomdamri@gmail.com>
2019-09-22 16:29:06 +0300

    installed - rc-slider, added - slider

--- color-picker-project/src/App.js
+++ color-picker-project/src/App.js
@@ -3,12 +3,7 @@ import Palette from './Palette/Palette'
 import seedColors from './seedColors/seedColors';
 import generatePalette from './seedColors/ColorScales';
 
-function App() {
-  let colors = generatePalette(seedColors[4]).colors.map( color =>{
-    console.log(color);
-  })
-  
-    
+function App() {  
   return (
     <div className="App">
       <Palette palette={generatePalette(seedColors[4])}/>
