414f0045904ee6ac7de7e2e318b7bab1abad5b63
TomDamri1 <tomdamri@gmail.com>
2019-09-23 14:08:08 +0300

    added - mini-palette , added some styles.

--- color-picker-project/src/PaletteList/PaletteList.js
+++ color-picker-project/src/PaletteList/PaletteList.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
+import MiniPalette from '../MiniPalette/MiniPalette';
 class PaletteList extends Component {
     render() {
         return (
             <div>
                 <h1>PaletteList</h1>
+                <MiniPalette/>
                 {this.props.palettes.map( p =>
                 <div>
-                    <Link to={`/palette/${p.id}`}>{p.paletteName}</Link>
+                    <MiniPalette {...p}/>
                 </div>
                 )}
             </div>
         )
     }
 }
-
+//<Link to={`/palette/${p.id}`}>{p.paletteName}</Link>
 export default PaletteList
 
