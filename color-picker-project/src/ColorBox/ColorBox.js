a302c5e769843028048359799fce7a7287aa9705
TomDamri1 <tomdamri@gmail.com>
2019-09-24 19:08:56 +0300

    refactor - ColorBox.css

--- color-picker-project/src/ColorBox/ColorBox.js
+++ color-picker-project/src/ColorBox/ColorBox.js
@@ -16,7 +16,8 @@ const styles={
         cursor: "pointer",
         marginBottom: "-3.5px",
         "&:hover button":{
-            opacity : 1,
+            opacity : '1',
+            transition: '0.5s',
         },
     },
     copyText:{
@@ -59,6 +60,63 @@ const styles={
         textDecoration: "none",
         opacity : '0',
     },
+    boxContent:{
+        position: 'absolute',
+        width: '100%',
+        bottom: '0px',
+        padding : '10px',
+        color:'black',
+        letterSpacing: '1px',
+        textTransform: 'uppercase',
+        fontSize: '12px',
+    },
+    copyOverlay:{
+        opacity: '0',
+        zIndex: '0',
+        width: '100%',
+        height:' 100%',
+        transition: 'transform 0.6s ease-in-out',
+        transform: 'scale(0.1)',
+    },
+    showOverlay:{
+        opacity: '1',
+        transform: 'scale(50)',
+        zIndex: '10',
+        position: 'absolute',
+    },
+    copyMessage:{
+        flexDirection: 'column',
+        position: 'fixed',
+        left: '0', 
+        right: '0',
+        bottom: '0',
+        top: '0',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '4rem',
+        transform: 'scale(0.1)',
+        opacity: '0',
+        color: 'white',
+        '& p' :{
+            fontSize: '2rem',
+            fontWeight: '100',
+        },
+        '& h1' :{
+            textAlign: 'center',
+            textShadow: '1px 2px black',
+            backgroundColor: 'rgba(255, 255, 255, 0.3)',
+            width: '100%',
+            textTransform: 'uppercase',
+        }
+    },
+    showCopyMessage:{
+        opacity: '1',
+        transform: 'scale(1)',
+        zIndex: '11',
+        transition: 'all 0.4s ease-in-out',
+        transitionDelay: '0.1s',
+    },
 }
 
 export class ColorBox extends Component {
@@ -87,14 +145,14 @@ export class ColorBox extends Component {
             text={this.props.background}
             onCopy={this.changeCopyState}>
             <div className={classes.ColorBox} style={{backgroundColor :background}}>
-                <div style={{backgroundColor :background}} className={`copy-overlay ${this.state.copied && 'show' }`}/>
-                <div className={`copy-msg ${this.state.copied && 'show' }`}>
+                <div style={{backgroundColor :background}} className={`${classes.copyOverlay} ${this.state.copied && classes.showOverlay }`}/>
+                <div className={`${classes.copyMessage} ${this.state.copied && classes.showCopyMessage }`}>
                     <h1 className={isLightColor && 'dark-text'}>Copied!</h1>
                     <p className={classes.copyText}>{name} , {this.props.background} </p>
                 </div>
 
-                <div className="copy-container">
-                    <div className="box-content">
+                <div>
+                    <div className={classes.boxContent}>
                         <span className={classes.colorName}>{name}</span>
                     </div>
                    <button className={classes.copyButton}>Copy!</button>
