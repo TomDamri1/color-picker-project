4fbaa46398af0bd5ba1be375091486c22eed5c37
TomDamri1 <tomdamri@gmail.com>
2019-09-25 16:52:00 +0300

    added - buttons, chrome-color-picker to Drawer

--- color-picker-project/src/NewPaletteForm/Drawer/PersistentDrawerLeft.js
+++ color-picker-project/src/NewPaletteForm/Drawer/PersistentDrawerLeft.js
@@ -11,9 +11,11 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import {ChromePicker} from 'react-color';
+import Button from '@material-ui/core/Button'
 
-const drawerWidth = 500;
-
+const drawerWidth = 400;
+let colorFromColorPicker = '';
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
@@ -123,6 +125,15 @@ export default function PersistentDrawerLeft() {
           </IconButton>
         </div>
         <Divider />
+        <Typography variant="h4">
+          Design Your Palette
+        </Typography>
+        <div>
+          <Button variant="contained" color="secondary">Clear Palette</Button>
+          <Button variant="contained" color="primary">Random Color</Button>
+        </div>
+        <ChromePicker onChangeComplete={(newColor)=> console.log(newColor)}/>
+        <Button variant="contained" color="primary">Add Color</Button>
         
       </Drawer>
       <main
